122b501e761b0d8cd936b58e24921757bba649fa
Felipe Cunha <felipe@cosmobots.io>
2021-04-08 15:52:47 -0300

    cria tabela com itens nao funcionais

--- src/Board.js
+++ src/Board.js
@@ -1,47 +1,35 @@
 import React, {Component} from "react";
 import Cell from "./Cell";
 import './Board.css';
-
-
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
 class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  }
 
   constructor(props) {
     super(props);
 
-    // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
   createBoard() {
     let board = [];
-    // TODO: create array-of-arrays of true/false values
+
+    for(let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+    }
+
     return board
   }
 
@@ -66,7 +54,7 @@ class Board extends Component {
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    // this.setState({board, hasWon});
   }
 
 
@@ -81,6 +69,23 @@ class Board extends Component {
     // make table board
 
     // TODO
+    let tableBoard = [];
+    for(let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++) {
+        row.push(<Cell isLit= {this.state.board[y][x]}/>)
+      }
+      tableBoard.push(<tr>{row}</tr>)
+    }
+    return (
+      <table className="Board">
+        <tbody>
+          <tr>
+            {tableBoard}
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 }
 
